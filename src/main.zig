dfbbd0078c80c4b6f55ac7f80ed9be1bd10a7028
Dmitry Gilemkhanov <dima.rize@yandex.ru>
2026-03-15 23:05:59 -0700

    Loop tcp echo

--- src/main.zig
+++ src/main.zig
@@ -2,7 +2,6 @@ const std = @import("std");
 const net = std.net;
 
 pub fn main() !void {
-    // listen on 0.0.0.0:8080
     const address = net.Address.parseIp("0.0.0.0", 8080) catch unreachable;
     var server = try address.listen(.{
         .reuse_address = true,
@@ -11,14 +10,15 @@ pub fn main() !void {
 
     std.debug.print("listening on :8080\n", .{});
 
-    // accept single connection
-    const conn = try server.accept();
-    defer conn.stream.close();
+    while (true) {
+        const conn = try server.accept();
+        defer conn.stream.close();
 
-    // read
-    var buf: [1024]u8 = undefined;
-    const n = try conn.stream.read(&buf);
-
-    // write again
-    _ = try conn.stream.write(buf[0..n]);
+        while (true) {
+            var buf: [1024]u8 = undefined;
+            const n = try conn.stream.read(&buf);
+            if (n == 0) break;
+            _ = try conn.stream.write(buf[0..n]);
+        }
+    }
 }
